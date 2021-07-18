423292c074e887a796c06e23595ffc5d4d19c4ee
brave123 <jjabkk6@naver.com>
2021-07-18 17:37:00 +0900

    두정수의 합

---
+++ 두정수의합.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include<iostream>
+using namespace std;
+
+long long solution(int a, int b)
+{
+    long long answer = 0;
+    if (a > b)
+    {
+        for (int i = a; i <= b; i++)
+        {
+            answer += i;
+        }
+    }
+    else
+    {
+        for (int i = b; i <= a; i++)
+        {
+            answer += i;
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    cout << solution(3, 5);
+    return 0;
+
+}
