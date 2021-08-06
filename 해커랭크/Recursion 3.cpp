6d437098fb9896e99a43a5c78a7b2c36642e75c4
brave123 <jjabkk6@naver.com>
2021-08-06 14:17:57 +0900

    해커랭크 recursion 3

---
+++ 해커랭크/Recursion 3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+/*
+ * Complete the 'factorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+int factorial(int n)
+{
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main()
+{
+   
+    int n;
+    cin >> n;
+    cout << factorial(n);
+    return 0;
+}
