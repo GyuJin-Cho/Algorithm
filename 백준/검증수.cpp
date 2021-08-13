2259e98c1fd57814f43d6bf86638e60b2b10dabd
brave123 <jjabkk6@naver.com>
2021-08-13 10:01:18 +0900

    백준 검증수

---
+++ 백준/검증수.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int sum = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> a;
+		sum += a*a;
+	}
+	sum = sum % 10;
+	cout << sum;
+}
