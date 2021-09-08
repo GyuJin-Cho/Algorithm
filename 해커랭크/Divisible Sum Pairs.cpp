6a3fe5d3d8cb69494f3306877c2ffe2aa5bd55d7
brave123 <jjabkk6@naver.com>
2021-09-08 18:45:59 +0900

    해커랭크/Divisible Sum Pairs

---
+++ 해커랭크/Divisible Sum Pairs.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int ar[101];
+	int n, k;
+	cin >> n >> k;
+	for (int i = 0;i < n; i++)
+	{
+		cin >> ar[i];
+	}
+	int count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i+1; j < n; j++)
+		{
+			if ((ar[i] + ar[j]) % k == 0)
+				count++;
+		}
+	}
+	cout << count;
+	return 0;
+}
