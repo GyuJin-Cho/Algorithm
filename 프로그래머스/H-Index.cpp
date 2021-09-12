9e5a1a7f448f6baf827ac7a68f18853293805754
brave123 <jjabkk6@naver.com>
2021-09-12 12:33:21 +0900

    프로그래머스/H-Index

---
+++ 프로그래머스/H-Index.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations)
+{
+    int answer = 0;
+    sort(citations.begin(), citations.end(), greater<int>());
+    if (citations[0] == 0)
+        return 0;
+    for(int i=0;i<citations.size();i++)
+    {
+        if (citations[i] > i)
+            answer++;
+        else
+            break;
+    }
+    return answer;
+}
+int main()
+{
+    vector<int> ci = { 3, 0, 6, 1, 5 };
+    cout << solution(ci);
+}
