a44a7ce221c8be21fec96cbb5efd818c9c254a2c
brave123 <jjabkk6@naver.com>
2021-10-02 16:20:11 +0900

    프로그래머스/자연수 뒤집어 배열로 만들기

---
+++ 프로그래머스/자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(long long n)
+{
+    vector<int> answer;
+    while (true)
+    {
+        answer.push_back(n % 10);  
+        n = n / 10;                
+        if (n == 0)
+        {
+            break;
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    vector<int> a = solution(12345);
+    for (auto i : a)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
