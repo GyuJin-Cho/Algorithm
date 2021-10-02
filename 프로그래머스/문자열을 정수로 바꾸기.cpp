cdd241646ed663cf85ae5972cebd69378ecd2486
brave123 <jjabkk6@naver.com>
2021-10-02 18:40:21 +0900

    프로그래머스/문자열을 정수로 바꾸기

---
+++ 프로그래머스/문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s)
+{
+    int answer = 0;
+    answer = stoi(s);
+    return answer;
+}
+
+int main()
+{
+    cout << solution("-1234");
+    return 0;
+}
