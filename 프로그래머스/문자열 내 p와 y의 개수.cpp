1df1fa1809255c3cb7991d408fca010a8b2149ac
brave123 <jjabkk6@naver.com>
2021-10-05 17:58:56 +0900

    프로그래머스/문자열 내 p와 y의 개수

---
+++ 프로그래머스/문자열 내 p와 y의 개수.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    
+
+    int pCount = 0;
+    int yCount = 0;
+    for(int i=0;i<s.length();i++)
+    {
+        if (s[i] == 'p' || s[i] == 'P')
+            pCount++;
+        else if (s[i] == 'y' || s[i] == 'Y')
+            yCount++;
+    }
+    if (pCount == yCount)
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    cout << solution("Pyy");
+    return 0;
+}
