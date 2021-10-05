bdaa02bdbcb622a3ee44ce0c563c931bb6227709
brave123 <jjabkk6@naver.com>
2021-10-05 17:21:46 +0900

    프로그래머스/숫자의 표현

---
+++ 프로그래머스/숫자의 표현.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0, nums = 1, sum = 0;
+    while (true) 
+    {
+        sum = 0;
+        for (int i = 1; i <= nums; i++)   
+            sum += i;
+        if (sum > n)  
+            break;
+        if ((n - sum) % nums++ == 0)  
+            answer++;
+    }
+    return answer;
+}
+
+int main()
+{
+    cout << solution(15);
+    return 0;
+}
