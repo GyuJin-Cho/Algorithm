0796c354c6d63e9e8fd72f73d6d3337449bc1e6a
brave123 <jjabkk6@naver.com>
2021-10-11 17:07:57 +0900

    프로그래머스/가장 큰 정사각형 찾기

---
+++ 프로그래머스/가장 큰 정사각형 찾기.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int solution(vector<vector<int>> board)
+{
+    int answer = board[0][0];
+    int r = board.size();
+    int c = board[0].size();
+    for (int i = 1; i < r; i++)
+    {
+        for (int j = 1; j < c; j++)
+        {
+            if (board[i][j] == 1)
+            {
+                board[i][j] = 1 + min({ board[i - 1][j - 1],board[i - 1][j],board[i][j - 1] });
+                answer = max(answer, board[i][j]);
+            }
+        }
+    }
+    answer *= answer;
+    return answer;
+}
+
+int main()
+{
+    vector<vector<int>> a = { {0,1,1,1},{1,1,1,1} ,{1,1,1,1},{0,0,1,0} };
+    cout << solution(a);
+    return 0;
+}
