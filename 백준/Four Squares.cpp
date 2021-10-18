1a44737330c1e55ef40e29b784b9cbdb8afa55d3
brave123 <jjabkk6@naver.com>
2021-10-19 00:05:53 +0900

    백준/Four Squares

---
+++ 백준/Four Squares.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int n;
+int DP[50001];
+
+int main()
+{
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+
+	DP[1] = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		DP[i] = DP[1] + DP[i - 1];
+		for (int j = 2; j * j <= i; j++)
+		{
+			DP[i] = min(DP[i], 1 + DP[i - j * j]);
+		}
+	}
+
+	cout << DP[n];
+	return 0;
+}
