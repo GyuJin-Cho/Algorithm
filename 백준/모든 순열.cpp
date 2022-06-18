342f86b6c469b227d46859dd907645c98ade2cb0
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2022-06-18 20:17:49 +0900

    모든 순열

---
+++ 백준/모든 순열.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int>v;
+	for (int i = 0; i < n; i++)
+	{
+		v.push_back(i + 1);
+	}
+
+	do
+	{
+		for (int i = 0; i < v.size(); i++)
+		{
+			if (i == v.size() - 1)
+				cout << v[i];
+			else
+				cout << v[i] << " ";
+		}
+		cout << '\n';
+
+
+	} while (next_permutation(v.begin(), v.end()));
+
+	return 0;
+}
