1bff3d359ae164f4bc8155fbb6df8e5cfbb36477
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2022-06-24 00:35:53 +0900

    백준 입출력 쉬운문제 커밋

---
+++ 백준/곱셈.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int a;
+	string b;
+	cin >> a >> b;
+
+	for (int i = 2; i >= 0; i--)
+	{
+		cout << a * (b[i] - '0')<<endl;
+	}
+	cout << a * stoi(b);
+	return 0;
+}
---
+++ 백준/백준10926.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    cin >> a;
+    a += "??!";
+    cout << a;
+
+    return 0;
+}
