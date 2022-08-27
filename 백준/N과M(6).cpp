3c44af50b024c77fdb8c7ef30011f0086254d22f
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2022-08-27 20:56:12 +0900

    백준 N과M(6)

---
+++ 백준/N과M(6).cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int n, m;
+int arr[9] = { 0, };
+int N[9] = { 0, };
+
+void DFS(int num,int Depth)
+{
+	if (Depth == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	else
+	{
+		for (int i = num; i <= n; i++)
+		{
+
+			arr[Depth] = N[i-1];
+			DFS(i+1, Depth + 1);
+		}
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> N[i];
+	}
+	sort(N, N + n);
+	DFS(1,0);
+	return 0;
+}
