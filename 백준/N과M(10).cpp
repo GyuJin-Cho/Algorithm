c588e4cf0eeed31bb40ac1a15e43333efa12d256
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2022-08-28 16:31:49 +0900

    백준 N과M(10)

---
+++ 백준/N과M(10).cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<set>
+
+using namespace std;
+
+int n, m;
+int arr[9] = { 0, };
+int N[9] = { 0, };
+vector<int> v;
+set<vector<int>> s;
+
+void DFS(int Num, int Depth)
+{
+	if (Depth == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			v.push_back(arr[i]);
+		}
+		s.insert(v);
+		v.clear();
+	}
+	else
+	{
+		for (int i = Num; i < n; i++)
+		{
+			arr[Depth] = N[i];
+			DFS(i+1,Depth + 1);
+		}
+	}
+}
+
+
+int main()
+{
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> N[i];
+	sort(N, N + n);
+	DFS(0,0);
+	for (auto vector : s)
+	{
+		for (auto set : vector)
+		{
+			cout << set<<" ";
+		}
+		cout << "\n";
+	}
+		
+
+	return 0;
+}
