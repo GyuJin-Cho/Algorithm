a437476d495aa8c3e2b42c1de3d1e5730f6c9c6c
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2022-10-02 18:53:39 +0900

    빠른A+B

---
+++ 백준/빠른A+B.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int main()
+{
+	int N;
+	scanf("%d",&N);
+	int a, b;
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d%d", &a, &b);
+		printf("%d\n", a + b);
+	}
+
+	return 0;
+}
