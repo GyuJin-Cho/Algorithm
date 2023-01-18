67cd9868a2d4c6ec919aabfce1c52625f44ab4fa
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-01-18 21:32:05 +0900

    종이 자르기

---
+++ 프로그래머스/종이 자르기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int M, int N)
+{
+    int answer = (M*N)-1;
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution(2, 5);
+
+	return 0;
+}
