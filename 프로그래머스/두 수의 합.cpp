7338f6804b3498fc12f7286a89eeea54a62b1095
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-01-26 20:59:50 +0900

    두 수의 합

---
+++ 프로그래머스/두 수의 합.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+int main()
+{
+    cout << solution(2, 3);
+
+	return 0;
+}
