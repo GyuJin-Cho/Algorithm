b967f31ebffbfe25f4a917397021f91b52476bb8
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-05-01 21:27:38 +0900

    문자열로 변환

---
+++ 프로그래머스/문자열로 변환.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n)
+{
+    return to_string(n);
+}
+
+int main()
+{
+    cout << solution(123);
+    return 0;
+}
