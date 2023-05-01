e38e541b773b403ddad2d9aa2cedcadfb732e1a6
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-05-01 16:36:40 +0900

    대문자로 바꾸기

---
+++ 프로그래머스/대문자로 바꾸기.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString)
+{
+    string answer = "";
+    
+    for(int i=0;i<myString.size();i++)
+    {
+        answer += toupper(myString[i]);
+    }
+    return answer;
+}
+
+int main()
+{
+    cout << solution("aBcDeFg");
+
+    return 0;
+}
