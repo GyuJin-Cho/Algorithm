a4b24a331fbbfb4778ce69cd7acba1f52e44b321
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-05-01 21:26:30 +0900

    문자열을 정수로 변환하기

---
+++ 프로그래머스/문자열을 정수로 변환하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string n_str)
+{
+    return stoi(n_str);
+}
+
+int main()
+{
+    cout << solution("10");
+    return 0;
+}
