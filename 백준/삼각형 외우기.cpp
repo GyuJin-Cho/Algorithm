fd078716418d3f3c5cabcbc6c27c2bb1b214e608
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-07-06 00:13:25 +0900

    삼각형 외우기

---
+++ 백준/삼각형 외우기.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	if(a+b+c!=180)
+	{
+		cout << "Error";
+	}
+	else
+	{
+		if(a==b||a==c||b==c)
+		{
+			if(a==b&&a==c&&b==c)
+			{
+				cout << "Equilateral";
+			}
+			else
+			{
+				cout << "Isosceles";
+			}
+			
+
+		}
+		else
+		{
+			cout << "Scalene";
+		}
+	}
+
+	return 0;
+}
