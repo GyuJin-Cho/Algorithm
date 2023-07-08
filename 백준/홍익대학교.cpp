89ff72937de761c1089eaeff2de18f9b8cad4d2d
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-07-08 19:35:10 +0900

    홍익대학교

---
+++ 백준/홍익대학교.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+
+using namespace std;
+
+int main()
+{
+	const int HongikBirth = 1946;
+	int var;
+	cin >> var;
+	cout << var - HongikBirth;
+
+	return 0;
+}
