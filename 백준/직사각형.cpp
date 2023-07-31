27d0f7fd49c8411273c88d3dbdcc3789cc6c5fb0
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-07-31 13:34:59 +0900

    직사각형

---
+++ 백준/직사각형.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a * b;
+
+	return 0;
+}
