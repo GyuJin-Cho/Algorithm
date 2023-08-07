6f811a412b644a95cd02741671a7f7a2bc8acec6
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-08-07 13:13:19 +0900

    보물

---
+++ 백준/보물.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+
+using namespace std;
+
+int main(void)
+{
+	int num;
+	int N;
+	cin >> N;
+	vector<int> a;
+	vector<int> b;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num;
+		a.push_back(num);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num;
+		b.push_back(num);
+	}
+
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end(), greater<>());
+	int res = 0;
+	for (int i = 0; i < N; i++) {
+		res += a[i] * b[i];
+	}
+	cout << res;
+}
