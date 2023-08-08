daf55fbf92d67fa1f95a8edeb365237b698cfe73
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-08-08 19:33:47 +0900

    네 번째 점

---
+++ 백준/네 번째 점.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+
+using namespace std;
+
+int main()
+{
+	int x[3];
+	int y[3];
+	for (int i = 0; i < 3; i++)
+		cin >> x[i] >> y[i];
+	if (x[0] == x[1])
+	{
+		cout << x[2] << ' ';
+	}
+	else if (x[0] == x[2])
+	{
+		cout << x[1] << ' ';
+	}
+	else
+	{
+		cout << x[0] << ' ';
+	}
+
+	if (y[0] == y[1])
+	{
+		cout << y[2] << ' ';
+	}
+	else if (y[0] == y[2])
+	{
+		cout << y[1] << ' ';
+	}
+	else
+	{
+		cout << y[0] << ' ';
+	}
+	return 0;
+}
