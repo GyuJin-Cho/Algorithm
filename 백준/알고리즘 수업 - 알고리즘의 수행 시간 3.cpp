371c719882cb475fe14d241ed4cab26d6ba63d20
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-08-08 21:29:42 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ 백준/알고리즘 수업 - 알고리즘의 수행 시간 3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+
+using namespace std;
+
+int main()
+{
+	long long n;
+	cin >> n;
+	cout << n*n << '\n' << 2;
+
+	return 0;
+}
