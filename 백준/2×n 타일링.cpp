3a69d00547a055365d57809c471c7ff6bed838d0
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-08-16 19:21:02 +0900

    2×n 타일링

---
+++ 백준/2×n 타일링.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+
+using namespace std;
+int d[1001];
+int main()
+{
+	int n;
+	cin >> n;
+	d[0] = 0;
+	d[1] = 1;
+	d[2] = 2;
+	for (int i = 3; i <= n; i++)
+	{
+		d[i] = d[i - 1] + d[i - 2];
+		d[i] %= 10007;
+	}
+	cout << d[n];
+	return 0;
+}
