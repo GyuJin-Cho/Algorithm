174fb027603f5dd507f149343baf6ba9c7b0e53a
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-08-17 17:46:51 +0900

    가장 긴 감소하는 부분 수열

---
+++ 백준/가장 긴 감소하는 부분 수열.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	vector<int> d(n);
+	for (int i = 0; i < n; i++)
+	{
+		d[i] = 1;
+		for (int j = 0; j < i; j++)
+		{
+			if (a[j]>a[i] && d[j] + 1>d[i])
+			{
+				d[i] = d[j] + 1;
+			}
+		}
+	}
+
+	cout << *max_element(d.begin(), d.end());
+
+	return 0;
+}
