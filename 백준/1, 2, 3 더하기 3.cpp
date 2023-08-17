610241b5d933bf51329395145c4a48f3fada2c98
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-08-17 23:24:49 +0900

    1, 2, 3 더하기 3

---
+++ 백준/1, 2, 3 더하기 3.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+
+using namespace std;
+long long D[1000002];
+const long long mod = 1000000009;
+int main()
+{
+	D[0] = 1;
+	for (int i = 1; i <= 1000000; i++)
+	{
+		if (D[i - 1] >= 0)
+		{
+			D[i] += D[i - 1];
+		}
+		if (D[i - 2] >= 0)
+		{
+			D[i] += D[i - 2];
+		}
+		if (D[i - 3] >= 0)
+		{
+			D[i] += D[i - 3];
+		}
+        D[i]%=mod;
+	}
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		cout << D[n]%mod<<'\n';
+	}
+	return 0;
+}
