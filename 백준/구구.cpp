88a50684e3a64ed469947bdc66b574bb3ff03d9c
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-09-11 17:28:30 +0900

    구구

---
+++ 백준/구구.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hymn To Love";
+	return 0;
+}
