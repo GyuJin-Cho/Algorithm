e9fc2e4951e8d18da7be3f88066d1a94f2f5a6e6
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-09-11 17:26:32 +0900

    정ㅋ벅ㅋ

---
+++ 백준/정ㅋ벅ㅋ.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+	cout << "문제의 정답";
+	return 0;
+}
