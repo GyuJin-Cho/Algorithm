dac5aa41a8e332832a0e32dbed2bebcec61a60db
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-09-11 17:30:45 +0900

    占쏙옙

---
+++ 백준/占쏙옙.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+	
+	cout << "�";
+
+	return 0;
+}
