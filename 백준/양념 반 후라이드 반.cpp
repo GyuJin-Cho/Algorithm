d21555610e207803210ec7627b02c42d43fe996e
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-09-15 18:52:10 +0900

    양념 반 후라이드 반

--- 백준/양념 반 후라이드 반.cpp
+++ 백준/양념 반 후라이드 반.cpp
@@ -21,8 +21,7 @@ int main()
     for (int i = 1; i <= 100000; i++)
     {
         int price = 2 * i * c + max(0, x - i) * a + max(0, y - i) * b;
-        if (ans > price) 
-            ans = price;
+        ans = min(ans, price);
     }
     cout << ans;
     return 0;
