65b7ed828f52e934952069b942905f9b906c7d52
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-09-15 18:51:19 +0900

    양념 반 후라이드 반

---
+++ 백준/양념 반 후라이드 반.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
+
+using namespace std;
+int main()
+{
+    int a, b, c, x, y;
+    cin >> a >> b >> c >> x >> y;
+    int ans = x * a + y * b;
+    for (int i = 1; i <= 100000; i++)
+    {
+        int price = 2 * i * c + max(0, x - i) * a + max(0, y - i) * b;
+        if (ans > price) 
+            ans = price;
+    }
+    cout << ans;
+    return 0;
+}
