0bb32a53ff3a146a9979ddef900f351ede064d72
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-10-11 00:07:40 +0900

    막대기(1094)

---
+++ 백준/막대기(1094).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cin >> x;
+
+	int ans = 0;
+	while(x>0)
+	{
+		if(x%2==1)
+		{
+			ans++;
+		}
+		x /= 2;
+	}
+	cout << ans;
+	return 0;
+}
