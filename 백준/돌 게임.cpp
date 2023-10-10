f8e6428a4ee78093c75dcdc0b8aeb5e803709865
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-10-11 00:17:08 +0900

    돌 게임

---
+++ 백준/돌 게임.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n % 2 == 1)
+		cout << "SK";
+	else
+		cout << "CY";
+
+	return 0;
+}
