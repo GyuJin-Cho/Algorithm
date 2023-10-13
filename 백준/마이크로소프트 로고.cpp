5d0c9c08bc420c4391d79aa5418ef38770ca916e
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-10-13 16:03:57 +0900

    마이크로소프트 로고

---
+++ 백준/마이크로소프트 로고.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+	cout << "       _.-;;-._" << '\n';
+	cout << "'-..-'|   ||   |" << '\n';
+	cout << "'-..-'|_.-;;-._|" << '\n';
+	cout << "'-..-'|   ||   |" << '\n';
+	cout << "'-..-'|_.-''-._|" << '\n';
+	return 0;
+}
