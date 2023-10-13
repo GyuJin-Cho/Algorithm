5376459618225f2f1fcbc3de480e8bd78f78f62c
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-10-13 16:06:17 +0900

    콜센터

---
+++ 백준/콜센터.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+	cout << "     /~\\" << '\n';
+	cout << "    ( oo|" << '\n';
+	cout << "    _\\=/_" << '\n';
+	cout << "   /  _  \\" << '\n';
+	cout << "  //|/.\\|\\\\" << '\n';
+	cout << " ||  \\ /  ||" << '\n';
+	cout << "============" << '\n';
+	cout << "|          |" << '\n';
+	cout << "|          |" << '\n';
+	cout << "|          |" << '\n';
+
+	return 0;
+}
