3f066045d7f86ca3e6f2a53a181c273f8c6eb1eb
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-10-13 16:01:12 +0900

    웰컴

---
+++ 백준/웰컴.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+	cout << ".  .   ." << '\n';
+	cout << "|  | _ | _. _ ._ _  _" << '\n';
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << '\n';
+	return 0;
+}
