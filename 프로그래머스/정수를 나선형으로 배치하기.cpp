6d90bd7a5d0542d8695ec20f926a82442cac3131
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-10-27 23:49:06 +0900

    정수를 나선형으로 배치하기

---
+++ 프로그래머스/정수를 나선형으로 배치하기.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(int n)
+{
+    vector<vector<int>> answer(n,vector<int>(n));
+    int num = 1;
+    int startY = 0, endY = n - 1;
+    int startX = 0, endX = n - 1;
+    while(num<=n*n)
+    {
+        for (int i = startY; i <= endY; ++i) {
+            answer[startX][i] = num++;
+        }
+        startX++;
+        for (int i = startX; i <= endX; ++i) {
+            answer[i][endY] = num++;
+        }
+        endY--;
+        for (int i = endY; i >= startY; --i) {
+            answer[endX][i] = num++;
+        }
+        endX--;
+        for (int i = endX; i >= startX; --i) {
+            answer[i][startY] = num++;
+        }
+        startY++;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<vector<int>> v = solution(4);
+    for(auto i : v)
+    {
+        for (auto j : i)
+            cout << j << ' ';
+        cout << '\n';
+    }
+        
+
+    return 0;
+}
