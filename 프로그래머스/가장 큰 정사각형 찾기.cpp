647b4f5a46984b8348012bf835dcd6b7c2988c7a
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-11-04 17:33:01 +0900

    가장 큰 정사각형 찾기

--- 프로그래머스/가장 큰 정사각형 찾기.cpp
+++ 프로그래머스/가장 큰 정사각형 찾기.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS 
+#include<iostream>
 #include<vector>
+#include<map>
 #include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
+
 using namespace std;
 
 int solution(vector<vector<int>> board)
@@ -12,20 +24,27 @@ int solution(vector<vector<int>> board)
     {
         for (int j = 1; j < c; j++)
         {
-            if (board[i][j] == 1)
+            if (board[i][j] != 0)
             {
                 board[i][j] = 1 + min({ board[i - 1][j - 1],board[i - 1][j],board[i][j - 1] });
                 answer = max(answer, board[i][j]);
             }
         }
     }
-    answer *= answer;
+
+    answer = answer* answer;
     return answer;
 }
 
 int main()
 {
-    vector<vector<int>> a = { {0,1,1,1},{1,1,1,1} ,{1,1,1,1},{0,0,1,0} };
-    cout << solution(a);
-    return 0;
+	int n, m;
+    cin >> n >> m;
+   
+	vector<vector<int>> v(n, vector<int>(m));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            scanf("%1d", &v[i][j]);
+    cout << solution(v);
+	return 0;
 }
