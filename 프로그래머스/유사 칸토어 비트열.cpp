16f8a3d38f6d2af1ab74e507e3bdbc1c32343987
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-11-05 17:16:52 +0900

    유사 칸토어 비트열

---
+++ 프로그래머스/유사 칸토어 비트열.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int div(long long n, long long l, long long r)
+{
+    if (l > r)
+        return 0;
+    if (n == 1)
+        return 1;
+    int ret = 0;
+    long long s = n / 5;
+    for(int i=0;i<5;i++)
+    {
+	    if(i==2)
+            continue;
+        ret += div(s, max(s * i, l) - (s * i), min(s * (i + 1) - 1, r) - (s * i));
+    }
+    return ret;
+}
+
+int solution(int n, long long l, long long r)
+{
+    return div(pow(5, n), l - 1, r - 1);
+}
+
+int main()
+{
+    cout << solution(2, 4, 17);
+
+    return 0;
+}
