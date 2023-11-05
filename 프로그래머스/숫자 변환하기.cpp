478bec427b43830c469f78c0f023832a7e6ca8b7
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-11-05 15:52:31 +0900

    숫자 변환하기

---
+++ 프로그래머스/숫자 변환하기.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+bool visit[1000001];
+
+int BFS(int x, int y, int n)
+{
+    visit[x] = true;
+    queue<pair<int, int>>q;
+    q.push({ x,0 });
+
+    while(!q.empty())
+    {
+        int now,cost;
+        tie(now, cost) = q.front();
+        q.pop();
+        if (now == y)
+            return cost;
+
+        if(now+n>=0&&now+n<=y)
+        {
+	        if(!visit[now+n])
+	        {
+                q.push({ now + n,cost + 1 });
+                visit[now + n] = true;
+	        }
+        }
+
+        if (now * 2 >= 0 && now * 2 <= y)
+        {
+            if (!visit[now * 2])
+            {
+                q.push({ now * 2,cost + 1 });
+                visit[now * 2] = true;
+            }
+        }
+
+        if (now * 3 >= 0 && now * 3 <= y)
+        {
+            if (!visit[now * 3])
+            {
+                q.push({ now * 3,cost + 1 });
+                visit[now * 3] = true;
+            }
+        }
+    }
+    return -1;
+}
+
+int solution(int x, int y, int n)
+{
+    int answer = BFS(x,y,n);
+
+
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution(2, 5, 4);
+
+    return 0;
+}
