c16d6466bad88c1c2258bc3d7331fff19367a1c6
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-11-07 20:55:57 +0900

    멀리 뛰기

---
+++ 프로그래머스/멀리 뛰기.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+int dp[2001];
+constexpr long long MOD = 1234567;
+long long solution(int n)
+{
+    long long answer = 0;
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(int i=3;i<=n;i++)
+    {
+        dp[i] = (dp[i - 1] + dp[i - 2]);
+    }
+    answer = dp[n];
+    return answer;
+}
+
+int main()
+{
+    cout << solution(5);
+
+    return 0;
+}
