d900840dbd236882483b1e41d663d9d6b4c195cd
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-11-17 21:10:47 +0900

    피보나치 수의 확장

--- 백준/피보나치 수의 확장.cpp
+++ 백준/피보나치 수의 확장.cpp
@@ -31,12 +31,12 @@ int main()
 
 	long long arr[MAX];
 	arr[0] = 0;
-	arr[1] = 0;
+	arr[1] = 1;
 
 	for(int i=2;i<=n;i++)
 	{
 		arr[i] = (arr[i - 1] + arr[i - 2]) % MOD;
 	}
-	cout << flag << '\n' << arr[n]%MOD;
+	cout << flag << '\n' << (arr[n]%MOD);
 	return 0;
 }
