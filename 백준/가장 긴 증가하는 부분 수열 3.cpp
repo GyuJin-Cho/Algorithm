4a807b73d364f46c5885be9286661e6c9a53d52b
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-11-20 20:35:10 +0900

    가장 긴 증가하는 부분 수열 3

---
+++ 백준/가장 긴 증가하는 부분 수열 3.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n, num;
+	cin >> n;
+	vector<int> arr;
+	vector<int> v;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num;
+		if (arr.empty() || arr.back() < num)
+		{
+			arr.push_back(num);
+			v.push_back(num);
+		}
+		else
+		{
+			vector<int>::iterator iter = lower_bound(arr.begin(), arr.end(), num);
+			*iter = num;
+		}
+	}
+
+	cout << arr.size() << '\n';
+	/*for (int i : v)
+		cout << i << ' ';*/
+	return 0;
+}
