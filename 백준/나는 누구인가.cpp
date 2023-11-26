97bb7c6b00457f96e8a356bbe36190fdbd2285ea
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-11-26 18:09:15 +0900

    나는 누구인가

---
+++ 백준/나는 누구인가.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+	cout << "I'm Sexy";
+
+	return 0;
+}
