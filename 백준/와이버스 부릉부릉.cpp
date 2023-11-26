1d55e67bd8258c0e9cb6a26a0ed49a21567b8c00
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-11-26 17:55:53 +0900

    와이버스 부릉부릉

---
+++ 백준/와이버스 부릉부릉.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
+
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	for(int i=0;i<n;i++)
+	{
+		int a, b;
+		cin >> a >> b;
+	}
+	cout << "ºñ¿ÍÀÌ";
+	return 0;
+}
