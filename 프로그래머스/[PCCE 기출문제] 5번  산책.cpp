f3b18ad8e511fd099e82fcb3d040fa8e77a2dcda
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2023-12-15 02:56:37 +0900

    [PCCE 기출문제] 5번  산책

---
+++ 프로그래머스/[PCCE 기출문제] 5번  산책.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string route) {
+    int east = 0;
+    int north = 0;
+    vector<int> answer(2);
+    for (int i = 0; i < route.length(); i++) {
+        switch (route[i]) {
+        case 'N':
+            north++;
+            break;
+        case 'S':
+
+            north--;
+            break;
+        case 'E':
+
+            east++;
+            break;
+        case 'W':
+
+            east--;
+
+            break;
+        }
+    }
+    answer[0] = east;
+    answer[1] = north;
+    return answer;
+}
+
+int main()
+{
+    vector<int> ans = solution("NSSNEWWN");
+    for (int i : ans)
+        cout << i << ' ';
+    return 0;
+}
