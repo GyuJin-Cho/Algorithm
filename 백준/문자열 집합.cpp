0ccc2a5c1ad061acd6cb263bcda09f0d5fd4b62a
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2024-01-26 22:09:02 +0900

    문자열 집합

--- 백준/문자열 집합.cpp
+++ 백준/문자열 집합.cpp
@@ -1,29 +1,39 @@
 #include<iostream>
+#include<vector>
 #include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
 
 using namespace std;
 
 int main()
 {
-	map<string, int> m;
-	int n, k;
-	cin >> n >> k;
-
-	string str;
-	for(int i=0;i<n;i++)
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int N, M;
+	cin >> N >> M;
+	unordered_set<string> s;
+	string input;
+	for(int i=0;i<N;i++)
 	{
-		cin >> str;
-		m[str]++;
+		cin >> input;
+		s.insert(input);
 	}
-	int cnt = 0;
-	for(int j=0;j<k;j++)
+	int ans = 0;
+	for(int i=0;i<M;i++)
 	{
-		cin >> str;
-		if (m[str] > 0)
-			cnt++;
+		cin >> input;
+		if (s.find(input) != s.end())
+			ans++;
 	}
-
-	cout << cnt;
-
+	cout << ans;
 	return 0;
 }
