9afb2a100c59420b94c849de039510f452d4405a
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2024-01-26 18:16:48 +0900

    숨바꼭질2

--- 백준/숨바꼭질2.cpp
+++ 백준/숨바꼭질2.cpp
@@ -1,12 +1,24 @@
 #include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
 #include<queue>
-using namespace std;
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
 
+using namespace std;
+constexpr int MAX = 100001;
 int dx[] = { 1,-1 };
-bool visit[100001] = { false, };
+bool visit[MAX] = { false, };
 void BFS(const int& N, const int& K)
 {
-	int ans = 0;
+
 	int SearchMax = 0;
 	queue<pair<int, int>> q;
 	q.push(make_pair(N, 0));
@@ -16,18 +28,17 @@ void BFS(const int& N, const int& K)
 	while (!q.empty())
 	{
 		pair<int, int> n = q.front();
- 		q.pop();
+		q.pop();
 		visit[n.first] = true;
 		if (n.first == K && check != 0 && minSec == n.second)
 			check++;
 
-		if (n.first == K && check==0)
+		if (n.first == K && check == 0)
 		{
-			cout << n.second<<'\n';
+			cout << n.second << '\n';
 			minSec = n.second;
 			check++;
 		}
-
 		if (2 * n.first < 100001)
 		{
 			if (!visit[2 * n.first])
@@ -49,12 +60,14 @@ void BFS(const int& N, const int& K)
 
 	}
 	cout << check;
+	
+	
 }
 
 int main()
 {
-	cin.tie(NULL);
-	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	int N, K;
 	cin >> N >> K;
 	BFS(N, K);
