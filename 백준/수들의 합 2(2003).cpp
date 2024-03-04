ab78341a3bc57862d14977c2aadf9c839312a74a
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2024-03-04 17:03:00 +0900

    수들의 합 2(2003)

--- 백준/수들의 합 2(2003).cpp
+++ 백준/수들의 합 2(2003).cpp
@@ -16,31 +16,35 @@ using namespace std;
 
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	int n, m;
 	cin >> n >> m;
-	vector<long long> v(n+1);
+	vector<int> v(n + 1);
 	for (int i = 0; i < n; i++)
+	{
 		cin >> v[i];
+	}
 	int start = -1;
 	int end = -1;
-	long long sum = 0;
+	int sum = 0;
 	int ans = 0;
-	while(start<=end&&end<n)
+	while (start <= end && end < n)
 	{
-		if(sum>m)
+		if (sum == m)
+		{
+			ans++;
+			start++;
+			sum -= v[start];
+		}
+		else if (sum > m)
 		{
 			start++;
 			sum -= v[start];
 		}
-		else if(sum<m)
+		else
 		{
 			end++;
-			sum += v[end];
-		}
-		else if(sum==m)
-		{
-			end++;
-			ans++;
 			sum += v[end];
 		}
 	}
