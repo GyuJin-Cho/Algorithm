756b11a8ccc85e5c5902e22493dbc5380b170910
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2024-04-07 10:11:51 +0900

    선 긋기

--- 백준/선 긋기.cpp
+++ 백준/선 긋기.cpp
@@ -22,24 +22,28 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
+	int n;
 	cin >> n;
-	for(int i=0;i<n;i++)
+	for (int i = 0; i < n; i++)
 	{
 		int a, b;
 		cin >> a >> b;
-		arr.push_back({ a,b });
+		arr.push_back(make_pair(a,b));
 	}
+
+
 	sort(arr.begin(), arr.end());
-	int ans = 0;
 	int start = arr[0].first;
 	int end = arr[0].second;
-	for(int i=1;i<n;i++)
+	int ans = 0;
+
+	for (int i = 1; i < arr.size(); i++)
 	{
 		int s = arr[i].first;
 		int e = arr[i].second;
-		if(end>=s)
+		if (end >= s)
 		{
-			if(end<e)
+			if (end < e)
 			{
 				end = e;
 			}
