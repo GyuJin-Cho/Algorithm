f9e7b3b8291788c6670cef88f3b1b1654324e0bd
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2024-04-10 21:14:54 +0900

    올림픽

--- 백준/올림픽.cpp
+++ 백준/올림픽.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 #include<vector>
+#include<map>
 #include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
 #include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
+
 using namespace std;
 
 class Medal
@@ -11,16 +21,15 @@ public:
 	int Silver;
 	int Bronze;
 public:
-	Medal() : Gold(0), Silver(0), Bronze(0){}
-	Medal(int g,int s, int b) : Gold(g), Silver(s), Bronze(b){}
-	
+	Medal() : Gold(0), Silver(0), Bronze(0) {}
+	Medal(int g, int s, int b) : Gold(g), Silver(s), Bronze(b) {}
 };
 
-bool cmp(pair<int,Medal> m1, pair<int,Medal> m2)
+bool cmp(pair<int, Medal> m1, pair<int, Medal> m2)
 {
-	if(m1.second.Gold==m2.second.Gold)
+	if (m1.second.Gold == m2.second.Gold)
 	{
-		if(m1.second.Silver==m2.second.Silver)
+		if (m1.second.Silver == m2.second.Silver)
 		{
 			return m1.second.Bronze > m2.second.Bronze;
 		}
@@ -31,36 +40,35 @@ bool cmp(pair<int,Medal> m1, pair<int,Medal> m2)
 
 int main()
 {
-	int N, K;
-	cin >> N >> K;
-	vector<pair<int, Medal>> country(N);
-
-	for(int i=0;i<N;i++)
+	int n, k;
+	cin >> n >> k;
+	vector<pair<int, Medal>> country;
+	for (int i = 0; i < n; i++)
 	{
-		int num,g,s,b;
+		int num, g, s, b;
 		cin >> num >> g >> s >> b;
-		country[i] = make_pair(num, Medal(g,s,b));
+		country.push_back(make_pair(num, Medal(g, s, b)));
 	}
 
-	sort(country.begin(), country.end(),cmp);
-	int result;
-	for(int i=0;i<country.size();i++)
+	sort(country.begin(), country.end(), cmp);
+	int number;
+	for (int i = 0; i < country.size(); i++)
 	{
-		if(country[i].first==K)
+		if (country[i].first == k)
 		{
-			result = i;
+			number = i;
 		}
 	}
 	int ans = 0;
-	for(int i=result-1;;i--)
+	for (int i = number - 1; i >= 0; i--)
 	{
-		if(country[i].second.Gold!=country[result].second.Gold||country[i].second.Silver!=country[result].second.Silver||
-			country[i].second.Bronze!=country[result].second.Bronze)
+		if (country[i].second.Gold != country[number].second.Gold || country[i].second.Silver != country[number].second.Silver ||
+			country[i].second.Bronze != country[number].second.Bronze)
 		{
 			break;
 		}
 		ans++;
 	}
-	cout << result - ans + 1;
+	cout << number - ans + 1;
 	return 0;
 }
