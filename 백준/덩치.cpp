6b58879d02a95dbffffb4f1baac03ee6b298ab78
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2024-04-10 19:53:12 +0900

    덩치

--- 백준/덩치.cpp
+++ 백준/덩치.cpp
@@ -1,47 +1,45 @@
 #include<iostream>
 #include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
 #include<tuple>
+
 using namespace std;
 
 int main()
 {
 	int N;
 	cin >> N;
-	vector<tuple<int, int , int>> v;
+	vector<tuple<int, int, int>>v;
+	int a, b;
 	for (int i = 0; i < N; i++)
 	{
-		int a;
-		int b;
 		cin >> a >> b;
-		v.push_back(make_tuple(a,b,1));
+		v.push_back(make_tuple(a, b, 1));
 	}
 
 	for (int i = 0; i < v.size(); i++)
 	{
-		for (int j = i + 1; j < v.size(); j++)
+		for (int j = 0; j < v.size(); j++)
 		{
+			if (i == j)
+				continue;
 			if (get<0>(v[i]) < get<0>(v[j]) && get<1>(v[i]) < get<1>(v[j]))
-			{
 				get<2>(v[i])++;
-			}
 		}
 	}
-
-	for (int i = v.size()-1; i >= 0; i--)
+	for (const auto& i : v)
 	{
-		for (int j = i - 1; j >=0; j--)
-		{
-			if (get<0>(v[i]) < get<0>(v[j]) && get<1>(v[i]) < get<1>(v[j]))
-			{
-				get<2>(v[i])++;
-			}
-		}
+		cout << get<2>(i) << ' ';
 	}
-
-	for (auto i : v)
-	{
-		cout << get<2>(i) << endl;;
-	}
-
 	return 0;
 }
