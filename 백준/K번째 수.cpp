45c4658efce03316234b8ec55adcfed15563fde5
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2024-06-26 20:56:20 +0900

    K번째 수

--- 백준/K번째 수.cpp
+++ 백준/K번째 수.cpp
@@ -16,17 +16,14 @@ using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
-	cout.tie(0);
-	int n, m;
-	cin >> n >> m;
-	vector<long long> v(n);
-	for(int i=0;i<n;i++)
-	{
-		cin >> v[i];
-	}
-	sort(v.begin(), v.end());
-	cout<<v[m - 1];
+	int n, k;
+	cin >> n >> k;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	sort(arr.begin(), arr.end());
+	cout << arr[k - 1];
 	return 0;
 }
