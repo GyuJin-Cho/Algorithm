025ddc11e6778c9865c5ffbbca7a584b1d241062
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2024-06-26 21:02:02 +0900

    수 정렬하기 3

--- 백준/수 정렬하기 3.cpp
+++ 백준/수 정렬하기 3.cpp
@@ -1,26 +1,32 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<unordered_set>
+#include<memory>
+#include<cstring>
 
 using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int T;
-    cin >> T;
-
-    int arr[10001] = { 0 };
-
-    for (int i = 0; i < T; i++) 
-    {
-        int a;
-        cin >> a;
-        arr[a] += 1;
-    }
-
-    for (int i = 1; i <= 10000; i++)
-        for (int j = 0; j < arr[i]; j++)
-            cout << i << "\n";
+	int n;
+	cin >> n;
+	vector<int>arr(10001,0);
+	for (int i = 0; i < n; i++)
+	{
+		int a;
+		cin >> a;
+		arr[a]++;
+	}
+	for (int i = 1; i <= 10000; i++)
+		for (int j = 0; j < arr[i]; j++)
+			cout << i << '\n';
+	return 0;
 }
