7d89ff91fb7f5721357bf48567154b8be6b7eb61
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2024-07-03 20:50:44 +0900

    소수 찾기

--- 프로그래머스/소수 찾기.cpp
+++ 프로그래머스/소수 찾기.cpp
@@ -1,49 +1,58 @@
+#include <iostream>
 #include <string>
 #include <vector>
-#include<iostream>
-#include<set>
-#include<algorithm>
+#include <algorithm>
+#include <set>
+#include <cstring>
 using namespace std;
+constexpr long long MAX = 10000000;
+bool check[MAX];
+
+void IsPrime()
+{
+    memset(check, true, sizeof(check));
+    check[0] = false;
+    check[1] = false;
+    for (int i = 2; i < MAX; i++)
+    {
+        if (check[i])
+        {
+            for (int j = i * 2; j < MAX; j+=i)
+            {
+                check[j] = false;
+            }
+        }
+    }
+}
 
 int solution(string numbers)
 {
     int answer = 0;
-    set<int> s;
+    IsPrime();
     sort(numbers.begin(), numbers.end());
+    set<int> s;
     do
     {
-        string temp = "";
-        for(int i=0;i<numbers.size();i++)
+        string tmp = "";
+        for (int i = 0; i < numbers.size(); i++)
         {
-            temp += numbers[i];
-            s.insert(stoi(temp));
+            tmp += numbers[i];
+            s.insert(stoi(tmp));
         }
     } while (next_permutation(numbers.begin(), numbers.end()));
-    int max = *s.rbegin();
-    vector<bool> check(4000000000, true);
-    check[0] = false;
-    check[1] = false;
 
-    for(int i=2;i*i<=max;i++)
+    for (const auto& i : s)
     {
-	    if(check[i])
-            for(int j=i*i;j<=max+1;j+=i)
-            {
-                check[j] = false;
-            }
-    }
-
-    for(auto iter = s.begin();iter!=s.end();iter++)
-    {
-        if (check[*iter])
+        if (check[i])
             answer++;
     }
+
     return answer;
 }
 
 int main()
 {
-    string a = "9999999";
-    cout << solution(a);
+    cout << solution("17");
+
     return 0;
 }
