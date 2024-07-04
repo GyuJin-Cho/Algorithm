41d93da9d2fcbda192a9b74f946736deb95c161c
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2024-07-04 17:20:44 +0900

    주식가격

--- 프로그래머스/주식가격.cpp
+++ 프로그래머스/주식가격.cpp
@@ -6,23 +6,23 @@ using namespace std;
 vector<int> solution(vector<int> prices)
 {
     vector<int> answer(prices.size());
-    for(int i=0;i<prices.size();i++)
+    for (int i = 0; i < prices.size(); i++)
     {
-	    for(int j=i+1;j<prices.size();j++)
-	    {
+        for (int j = i + 1; j < prices.size(); j++)
+        {
             answer[i]++;
             if (prices[i] > prices[j])
                 break;
-	    }
+        }
     }
     return answer;
 }
 
 int main()
 {
-    vector<int> prices = { 1,2,3,2,3 };
-    vector<int> v = solution(prices);
-    for (auto a : v)
-        cout << a;
-	return 0;
+    vector<int> v = { 1,2,3,2,3 };
+    vector<int> ans = solution(v);
+    for (const int& i : ans)
+        cout << i << ' ';
+    return 0;
 }
