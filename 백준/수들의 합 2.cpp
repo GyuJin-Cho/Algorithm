1590fccf72a1749f133f5648472536d555cf75ed
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2025-09-14 19:17:29 +0900

    Create 수들의 합 2.cpp

---
+++ 백준/수들의 합 2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N,M;
+    cin>>N>>M;
+    vector<int> v(N+1);
+    for(int i=0;i<N;i++)
+    {
+        cin>>v[i];
+    }
+
+    int start = -1;
+    int end = -1;
+    int ans = 0;
+    int sum = 0;
+
+    while(start <= end && end < N)
+    {
+        if(sum == M)
+        {
+            ans++;
+            start++;
+            sum -= v[start];
+        }
+        else if(sum > M)
+        {
+            start++;
+            sum -= v[start];
+        }
+        else
+        {
+            end++;
+            sum += v[end];
+        }
+    }
+    cout<<ans;
+    return 0;
+}
