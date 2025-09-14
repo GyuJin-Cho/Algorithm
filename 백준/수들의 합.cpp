352329a4e2e9473710a5a9de5ec2f8d70f8f1ab4
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2025-09-14 19:55:34 +0900

    Update 수들의 합.cpp

--- 백준/수들의 합.cpp
+++ 백준/수들의 합.cpp
@@ -1,27 +1,30 @@
-#include<iostream>
+﻿#include<iostream>
 
 using namespace std;
 
 int main()
 {
-	cin.tie(0);
-	cout.tie(0);
-	long long n;
-	cin >> n;
-	long long sum = 0;
-	int num = 1;
-	int count = 0;
-	while (true)
-	{
-		sum += num;
-		count++;
-		if (sum > n)
-		{
-			count--;
-			break;
-		}
-		num++;
-	}
-	cout << count;
-	return 0;
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    unsigned long long s;
+    cin>>s;
+
+    unsigned long long num = 1;
+    unsigned long long sum = 0;
+    unsigned long long cnt = 0;
+
+    while(1)
+    {
+        sum += num;
+        cnt++;
+        if(sum > s)
+        {
+            cnt--;
+            break;
+        }
+        num++;
+    }
+    cout<<cnt;
 }
