692a46d57af0e73e95bf0f46ee55fe17677aa936
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2025-09-14 17:45:48 +0900

    Create 택배 상자 꺼내기.cpp

---
+++ 프로그래머스/택배 상자 꺼내기.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int solution(int n, int w, int num)
+{
+    int row = (num - 1) / w;
+    int col = (num - 1) % w;
+
+    if (row % 2 == 1)
+    {
+        col = w - 1 - col;
+    }
+
+    int count = 1;
+
+    int r = row + 1;
+    while (r * w < n)
+    { 
+        int start = r * w + 1;
+        int target;
+        if (r % 2 == 0)
+        {
+            target = start + col;
+        }
+        else
+        {
+            target = start + (w - 1 - col);
+        }
+        if (target <= n)
+        {   
+            count++;
+        }
+        r++;
+    }
+
+    return count;
+}
+
+int main() {
+    cout << solution(22, 6, 2) << endl;
+    cout << solution(13, 3, 6) << endl;
+    return 0;
+}
