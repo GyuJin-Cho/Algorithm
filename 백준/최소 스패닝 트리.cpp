40dd6f271d06369ecca30cdad6be3355460ec6ff
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2025-09-16 22:09:37 +0900

    Update 최소 스패닝 트리.cpp

--- 백준/최소 스패닝 트리.cpp
+++ 백준/최소 스패닝 트리.cpp
@@ -1,55 +1,56 @@
-#include<iostream>
+﻿#include<iostream>
 #include<queue>
 #include<vector>
 
 using namespace std;
-vector<pair<int, int>> N[10001];
 
-bool visit[10001] = { false, };
+int N,M;
+vector<pair<long long,long long>> Edge[10001];
+bool Visited[10001];
+long long ans = 0;
+
 void MST()
 {
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-	pq.push(make_pair(0, 1));
-	long long answer = 0;
-	while (!pq.empty())
-	{
-		int Cost = pq.top().first;
-		int Now_node = pq.top().second;
-		pq.pop();
-		if (visit[Now_node])
-		{
-			continue;
-		}
-		visit[Now_node] = true;
-		answer += Cost;
-		for (int i = 0; i < N[Now_node].size(); i++)
-		{
-			if (!visit[N[Now_node][i].first])
-			{
-				int Next_Node = N[Now_node][i].first;
-				int Next_Cost = N[Now_node][i].second;
-				pq.push(make_pair(Next_Cost, Next_Node));
-			}
+    priority_queue<pair<long long,long long>, vector<pair<long long, long long>>, greater<pair<long long,long long>>> pq;
 
-		}
-	}
-	cout << answer;
+    pq.push({0,1});
+    while(!pq.empty())
+    {
+        long long NowNode = pq.top().second;
+        long long NowCost = pq.top().first;
+        pq.pop();
+        if(Visited[NowNode])
+            continue;
+        Visited[NowNode] = true;
+        ans += NowCost;
+        for(int i=0; i<Edge[NowNode].size();i++)
+        {
+            int nCost = Edge[NowNode][i].second;
+            int nNode = Edge[NowNode][i].first;
+            if(!Visited[nNode])
+            {
+                pq.push({nCost,nNode});
+            }
+        }
+    }
 }
 
 int main()
 {
-	int V, E;
-	cin >> V >> E;
-	for (int i = 0; i < E; i++)
-	{
-		int A, B, C;
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin>>N>>M;
+    for(int i=0;i<M;i++)
+    {
+        long long u,v,c;
+        cin>>u>>v>>c;
+        Edge[u].push_back(make_pair(v,c));
+        Edge[v].push_back(make_pair(u,c));
+    }
 
-		cin >> A >> B >> C;
-		N[A].push_back(make_pair(B, C));
-		N[B].push_back(make_pair(A, C));
-	}
-
-
-	MST();
-	return 0;
+    MST();
+    
+    cout<<ans;
+    return 0;
 }
