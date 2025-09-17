d203afd5e528d5a4637814f10962495571193aa8
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2025-09-17 21:35:12 +0900

    Update 괄호.cpp

--- 백준/괄호.cpp
+++ 백준/괄호.cpp
@@ -1,34 +1,40 @@
-#include <iostream>
-#include <stack>
-#include <string>
+﻿#include<iostream>
+#include<stack>
 
 using namespace std;
 
 int main()
 {
-	int T = 0;
-
-	cin >> T;
-
-	for (int i = 0; i < T; ++i)
-	{
-		stack<char> st;
-		string s;
-		cin >> s;
-
-		for (int j = 0; j < s.length(); ++j)
-		{
-			if (st.empty() || s[j] == '(')
-				st.push(s[j]);
-			else if (st.top() == '(')
-				st.pop();
-		}
-
-		if (st.empty()) 
-			cout << "YES" << endl;
-		else 
-			cout << "NO" << endl;
-	}
-
-	return 0;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        stack<char> s;
+        string p;
+        cin >> p;
+        for (size_t j = 0; j < p.length(); j++)
+        {
+            if (p[j] == '(')
+                s.push('(');
+            else
+            {
+                if (!s.empty())
+                {
+                    if (s.top() == '(')
+                        s.pop();
+                    else
+                    {
+                        s.push(p[j]);
+                    }
+                }
+                else
+                    s.push(p[j]);
+            }
+        }
+        if (!s.empty())
+            cout << "NO" << '\n';
+        else
+            cout << "YES" << '\n';
+    }
+    return 0;
 }
