beda125627f6a8d8a58aa68200d14c8daae658c6
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2025-09-17 23:00:54 +0900

    Update 미로 탐색.cpp

--- 백준/미로 탐색.cpp
+++ 백준/미로 탐색.cpp
@@ -1,79 +1,72 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include<algorithm>
-#include<cmath>
-#include<string>
+﻿#include<iostream>
 #include<queue>
-#include<stack>
-#include<set>
-#include<unordered_map>
-#include<unordered_set>
-#include<memory>
-#include<cstring>
-
+#include<array>
 using namespace std;
-int arr[101][101];
-bool visit[101][101];
-int dx[] = { 1,0,-1,0 };
-int dy[] = { 0,1,0,-1 };
-int n, m;
 
-class node
-{
-public:
-	int y;
-	int x;
-	int cnt;
-	node(int y, int x,int cnt) : y(y), x(x),cnt(cnt){}
-	node():	y(0) , x(0),cnt(0){}
-	
-};
+int N,M;
 
-void BFS(int y, int x)
+vector<vector<int>> Map;
+vector<vector<bool>> Visited;
+vector<vector<int>> ArrCnt;
+array<int,4> Dy = {0,1,0,-1};
+array<int,4> Dx = {1,0,-1,0};
+constexpr int Cicle = 4;
+void BFS()
 {
-	queue<node> q;
-	q.push(node(y, x, 1));
-	visit[y][x] = true;
-	while (!q.empty())
-	{
-		node no = q.front();
-		q.pop();
-		if (no.y == n - 1 && no.x == m - 1)
-		{
-			cout << no.cnt;
-			return;
-		}
-		for (int i = 0; i < 4; i++)
-		{
-			int ny = no.y + dy[i];
-			int nx = no.x + dx[i];
-			if (ny >= 0 && nx >= 0 && ny < n && nx < m)
-			{
-				if (!visit[ny][nx]&&arr[ny][nx]==1)
-				{
-					q.push(node(ny, nx, no.cnt + 1));
-					visit[ny][nx] = true;
-				}
-			}
-		}
-	}
+    queue<pair<int,int>> q;
+    q.push(make_pair(0,0));
+    Visited[0][0] = true;
+    ArrCnt[0][0] = 1;
+    while(!q.empty())
+    {
+        int NowY = q.front().first;
+        int NowX = q.front().second;
+        q.pop();
+        if(NowY == N-1 && NowX == M-1)
+        {
+            cout<<ArrCnt[NowY][NowX];
+            return;
+        }
+        for(int i=0;i<Cicle;i++)
+        {
+            int NextY = NowY + Dy[i];
+            int NextX = NowX + Dx[i];
+            if(NextX >= 0 && NextY >=0 && NextX < M && NextY < N)
+            {
+                if(!Visited[NextY][NextX] && Map[NextY][NextX] == 1)
+                {
+                    q.push(make_pair(NextY,NextX));
+                    Visited[NextY][NextX] = true;
+                    ArrCnt[NextY][NextX] = ArrCnt[NowY][NowX] + 1;
+                }
+            }
+        }
+    }
 }
 
 int main()
 {
-	cin >> n >> m;
-	string s;
-	for(int i=0;i<n;i++)
-	{
-		cin >> s;
-		for (int j = 0; j < s.size(); j++)
-		{
-			arr[i][j] = (s[j] - 48);
-		}
-	}
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin>>N>>M;
+    Map.resize(N + 1);
+    Visited.resize(N + 1);
+    ArrCnt.resize(N + 1);
+    for(int i=0;i<N;i++)
+    {
+        Map[i].resize(M + 1);
+        Visited[i].resize(M + 1);
+        ArrCnt[i].resize(M + 1);
+        string temp;
+        cin>>temp;
+        for(size_t j=0;j<temp.length();j++)
+        {
+            Map[i][j] = temp[j] - 48;
+        }
+    }
 
-	BFS(0, 0);
-
-	return 0;
+    BFS();
+    
+    return 0;
 }
