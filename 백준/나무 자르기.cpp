b7078412e36fe819f23e8ab7520c28856005ac20
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2025-09-20 01:08:26 +0900

    Update 나무 자르기.cpp

--- 백준/나무 자르기.cpp
+++ 백준/나무 자르기.cpp
@@ -1,61 +1,44 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include<algorithm>
-#include<cmath>
-#include<string>
-#include<queue>
-#include<stack>
-#include<set>
-#include<unordered_map>
-#include<unordered_set>
-#include<memory>
-#include<cstring>
+﻿#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int n, m;
-	cin >> n >> m;
-	vector<int> v(n);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> v[i];
-	}
-	sort(v.begin(), v.end());
-	long long start = 0;
-	long long end = v[n-1];
-	long long ans = -21740000;
-	
-	while (start <= end)
-	{
-		long long mid = (start + end) / 2;
-		long long sum = 0;
-		for (int i = 0; i < n; i++)
-		{
-			if (v[i] - mid > 0)
-				sum += (v[i] - mid);
-		}
-		if (sum > m)
-		{
-			start = mid + 1;
-			if (ans < mid)
-				ans = mid;
-		}
-		else if (sum < m)
-		{
-			end = mid - 1;
-		}
-		else if (sum == m)
-		{
-			start = mid + 1;
-			if (ans < mid)
-				ans = mid;
-		}
-	}
-	cout << ans;
-	return 0;
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    
+    int n,m;
+    cin>>n>>m;
+    vector<long long> v(n);
+    for(int i=0;i<n;i++)
+        cin>>v[i];
+
+    sort(v.begin(),v.end());
+
+    long long start = 0;
+    long long end = v[n-1];
+    long long ans = 0;
+    
+    while(start<=end)
+    {
+        long long mid = (start+end)/2;
+        long long sum = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(v[i]-mid > 0)
+                sum = sum + (v[i] - mid);
+        }
+        if(sum >= m)
+        {
+            ans = mid;
+            start = mid+1;
+        }
+        else if(sum < m)
+        {
+            end = mid-1;
+        }
+    }
+    cout<<ans;
+    return 0;
 }
