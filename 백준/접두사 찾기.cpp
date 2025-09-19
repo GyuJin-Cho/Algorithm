df22b29ae4d489213dc49c9e3fbaaa8a0a3e85c8
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2025-09-20 00:09:48 +0900

    Update 접두사 찾기.cpp

--- 백준/접두사 찾기.cpp
+++ 백준/접두사 찾기.cpp
@@ -1,92 +1,80 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include<algorithm>
-#include<cmath>
-#include<string>
-#include<queue>
-#include<stack>
-#include<set>
-#include<unordered_map>
-#include<unordered_set>
-#include<memory>
-#include<cstring>
+﻿#include <bits/stdc++.h>
 
 using namespace std;
-
+#define MAX 26
 class Trie
 {
 public:
-	Trie* node[26];
+    Trie()
+    {
+        Finish = false;
+        for(int i=0;i<MAX;i++)
+            Node[i] = nullptr;
+    }
 
-	Trie()
-	{
-		for (int i = 0; i < 26; i++)
-			node[i] = nullptr;
-	}
+    ~Trie()
+    {
+        for(int i=0;i<MAX;i++)
+        {
+            delete Node[i];
+            Node[i] = nullptr;
+        }
+    }
 
-	void insert(const string& str, int idx)
-	{
-		if (idx == str.length())
-			return;
+    void Insert(const string& _s, int _idx)
+    {
+        if(_s.length() == _idx)
+        {
+            Finish = true;
+            return;
+        }
 
-		if(node[str[idx]-'a']==nullptr)
-			node[str[idx] - 'a'] = new Trie();
-		
-		node[str[idx] - 'a']->insert(str, idx + 1);
-	}
+        if(!Node[_s[_idx]-'a'])
+            Node[_s[_idx]-'a'] = new Trie();
+        Node[_s[_idx]-'a']->Insert(_s,_idx+1);
+    }
 
-	bool find (const string& str, int idx)
-	{
-		if (idx == str.length())
-			return true;
+    bool Find(const string& _s, int _idx)
+    {
+        if(_s.length() == _idx)
+            return true;
+        if(Finish)
+            return true;
+        if(!Node[_s[_idx]-'a'])
+            return false;
 
-		if (node[str[idx] - 'a'] == nullptr)
-			return false;
+        return Node[_s[_idx]-'a']->Find(_s,_idx+1);
+    }
 
-		return node[str[idx] - 'a']->find(str, idx + 1);
-	}
-
-	void Clear()
-	{
-		for(int i=0;i<26;i++)
-		{
-			if(node[i])
-			{
-				node[i]->Clear();
-				delete node[i];
-			}
-		}
-	}
+private:
+    Trie* Node[MAX];
+    bool Finish;
 };
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+    int n,m;
+    cin>>n>>m;
+    Trie* trie = new Trie();
+    string s;
+    for(int i=0;i<n;i++)
+    {
+        cin>>s;
+        trie->Insert(s,0);
+    }
 
-	int n, m;
-	cin >> n >> m;
-	string str;
+    int ans=0;
+    for(int i=0;i<m;i++)
+    {
+        cin>>s;
+        
+        if(trie->Find(s,0))
+        {
+            ans++;
+        }
+    }
 
-	Trie* root = new Trie();
-
-	for(int i=0;i<n;i++)
-	{
-		cin >> str;
-		root->insert(str,0);
-	}
-
-	int ans = 0;
-	for(int i=0;i<m;i++)
-	{
-		cin >> str;
-
-		if (root->find(str, 0))
-			ans++;
-	}
-	cout << ans;
-	root->Clear();
-	delete(root);
-	return 0;
+    cout<<ans;
+    delete trie;
+    return 0;
 }
