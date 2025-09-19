11887dfdf1c8902f2b14c36cdc7e0eee1b01713b
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2025-09-19 20:04:05 +0900

    Update 회의실 배정.cpp

--- 백준/회의실 배정.cpp
+++ 백준/회의실 배정.cpp
@@ -1,48 +1,42 @@
-#include<iostream>
+﻿#include<iostream>
 #include<vector>
-#include<map>
 #include<algorithm>
-#include<cmath>
-#include<string>
-#include<queue>
-#include<stack>
-#include<set>
-#include<unordered_map>
-#include<unordered_set>
-#include<memory>
-#include<cstring>
 
 using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	vector<pair<int, int>> v;
-	for(int i=0;i<n;i++)
-	{
-		int s, e;
-		cin >> s >> e;
-		v.push_back({ s,e });
-	}
-	sort(v.begin(), v.end(), [](const pair<int, int>& p1, const pair<int, int>& p2)
-		{
-			if (p1.second == p2.second)
-				return p1.first < p2.first;
-			return p1.second < p2.second;
-		});
-	int ch = 0;
-	int ans = 0;
-	for(int i=0;i<v.size();i++)
-	{
-		if(ch<=v[i].first)
-		{
-			ch = v[i].second;
-			ans++;
-		}
-	}
-	cout << ans;
-	return 0;
+    int n;
+    cin>>n;
+
+    vector<pair<int,int>> arr;
+
+    for(int i=0;i<n;i++)
+    {
+        int s,e;
+        cin>>s>>e;
+        arr.push_back(make_pair(s,e));
+    }
+
+    sort(arr.begin(),arr.end(),[](const pair<int,int>& _Left, const pair<int,int>& _Right)
+    {
+        if(_Left.second == _Right.second)
+            return _Left.first < _Right.first;
+        return _Left.second < _Right.second;
+    });
+
+    int temp = 0;
+    int cnt = 0;
+    for(int i=0;i<arr.size();i++)
+    {
+        if(temp<=arr[i].first)
+        {
+            temp = arr[i].second;
+            cnt++;
+        }
+    }
+
+    cout<<cnt;
+    
+    return 0;
 }
