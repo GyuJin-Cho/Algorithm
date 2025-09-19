cda85f0c6eeb1e959eb4d4bd5b63fec7622aeabd
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2025-09-20 00:18:16 +0900

    Update 개미굴.cpp

--- 백준/개미굴.cpp
+++ 백준/개미굴.cpp
@@ -1,67 +1,58 @@
-#include<iostream>
-#include<vector>
-#include<map>
-#include<algorithm>
-#include<cmath>
-#include<string>
-#include<queue>
-#include<stack>
-#include<set>
-#include<unordered_map>
-#include<unordered_set>
-#include<memory>
-#include<cstring>
+﻿#include <bits/stdc++.h>
 
 using namespace std;
 
 class Trie
 {
 private:
-	map<string, Trie*>node;
-
+    map<string, unique_ptr<Trie>> Node;
 public:
 
-	void Insert(const vector<string>& foods, int idx)
-	{
-		if (idx == foods.size())
-			return;
-
-		if(node.find(foods[idx])==node.end())
-			node[foods[idx]] = new Trie();
-		node[foods[idx]]->Insert(foods, idx + 1);
-	}
-
-	void Print(int depth)
-	{
-		for(auto& no : node)
-		{
-			for (int i = 0; i < depth; i++)
-				cout << "--";
-			cout << no.first << '\n';
-			no.second->Print(depth + 1);
-		}
-	}
+    ~Trie()
+    {
+        Node.clear();
+    }
+    void Insert(const vector<string>& v, int idx)
+    {
+        if (v.size() == idx)
+            return;
+        if (!Node[v[idx]])
+            Node[v[idx]] = make_unique<Trie>();
+        Node[v[idx]]->Insert(v, idx + 1);
+    }
+    void Print(int Depth)
+    {
+        for (const auto& no : Node)
+        {
+            for (int i = 0; i < Depth; i++)
+            {
+                cout << "--";
+            }
+            cout << no.first << '\n';
+            no.second->Print(Depth + 1);
+        }
+    }
 };
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int T;
-	cin >> T;
+    int T;
+    cin >> T;
+    Trie* root = new Trie();
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        vector<string> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        root->Insert(v, 0);
 
-	Trie* root = new Trie();
-	while(T--)
-	{
-		int n;
-		cin >> n;
-		vector<string> foods(n);
-		for(int i=0;i<n;i++)
-			cin >> foods[i];
+    }
+    root->Print(0);
+    delete root;
 
-		root->Insert(foods, 0);
-	}
-
-	root->Print(0);
-	return 0;
+    return 0;
 }
