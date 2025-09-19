ff9cafb72d614fdca8885496a70252863d3e5df8
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2025-09-19 20:11:38 +0900

    Update ATM.cpp

--- 백준/ATM.cpp
+++ 백준/ATM.cpp
@@ -1,43 +1,35 @@
-#include<iostream>
-#include<vector>
-#include<map>
+﻿#include<iostream>
 #include<algorithm>
-#include<cmath>
-#include<string>
-#include<queue>
-#include<stack>
-#include<set>
-#include<unordered_map>
-#include<unordered_set>
-#include<memory>
-#include<cstring>
+#include<vector>
 
 using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	vector<int> v(n);
-	for (int i = 0; i < n; i++)
-		cin >> v[i];
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
+        cin>>v[i];
 
-	sort(v.begin(), v.end());
-	int sum = 0;
-	vector<int> v1;
-	for (int i = 0; i < n; i++)
-	{
-		sum += v[i];
-		v1.push_back(sum);
-	}
+    sort(v.begin(),v.end());
 
-	int ans = 0;
-	for (int i = 0; i < n; i++)
-	{
-		ans += v1[i];
-	}
-	cout << ans;
-	return 0;
+    vector<int> temp(n);
+    int sum = 0;
+    
+    for(int i=0;i<v.size();i++)
+    {
+        sum+=v[i];
+        temp[i] = sum;
+    }
+    
+    int answer = 0;
+    for(int i=0;i<temp.size();i++)
+    {
+        answer+=temp[i];
+    }
+
+    cout<<answer;
+    
+    return 0;
 }
