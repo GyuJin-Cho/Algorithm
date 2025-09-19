28592f1bac66a4191d5ac6b913bb147e0fa393f1
GyuJin-Cho <54353036+GyuJin-Cho@users.noreply.github.com>
2025-09-19 23:55:29 +0900

    Update 전화번호 목록.cpp

--- 백준/전화번호 목록.cpp
+++ 백준/전화번호 목록.cpp
@@ -1,69 +1,93 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+﻿#include <bits/stdc++.h>
+
 using namespace std;
 
-string map[1000001];
-
-bool solution(vector<string> phone_book)
+#define MAX 10
+class Trie
 {
-	bool answer = true;
-	int phone_size = phone_book.size();
-	sort(phone_book.begin(), phone_book.end());
-	for (int i = 0; i < phone_size - 1; i++)
-	{
-		int check_size = 0;
-		string s = phone_book[i];
-		int s_size = s.size();
+public:
 
-		string s_check = phone_book[i + 1];
-		int s_check_size = s_check.size();
-		for (int j = 0; j < s_check_size; j = j + s_size)
-		{
-			map[check_size] = s_check.substr(j, s_size);
-			check_size++;
-		}
-		for (int j = 0; j < check_size; j++)
-		{
-			if (map[j] == s)
-			{
-				return false;
-			}
-			else
-			{
-				continue;
-			}
-		}
-	}
-	return answer;
-}
+    Trie()
+    {
+        Finish = false;
+        for(int i=0;i<MAX;i++)
+        {
+            Node[i] = nullptr;
+        }
+    }
+
+    ~Trie()
+    {
+        for(int i=0;i<MAX;i++)
+        {
+            if(Node[i])
+            {
+                delete Node[i];
+                Node[i] = nullptr;
+            }
+        }
+    }
+    
+    void Insert(const string& s, int idx)
+    {
+        if(s.length() == idx)
+        {
+            Finish = true;
+            return;
+        }
+
+        if(!Node[s[idx]-48])
+            Node[s[idx]-48] = new Trie();
+        Node[s[idx]-48]->Insert(s, idx+1);
+    }
+
+    bool Find(const string& s, int idx)
+    {
+        if(s.length() == idx)
+            return false;
+        if(Finish)
+            return true;
+        if(!Node[s[idx]-48])
+            return false;
+        return Node[s[idx]-48]->Find(s, idx+1);
+    }
+
+private:
+    Trie* Node[MAX];
+    bool Finish;
+};
 
 int main()
 {
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int t;
-	cin >> t;
-	
-	for (int i = 0; i < t; i++)
-	{
-		bool check = false;
-		int n;
-		cin >> n;
-		vector<string> s;
-		for (int j = 0; j < n; j++)
-		{
-			string a;
-			cin >> a;
-			s.push_back(a);
-		}
-		sort(s.begin(), s.end());
-		
-		if (!solution(s))
-			cout << "NO" << '\n';
-		else
-			cout << "YES" << '\n';
-	}
+    int t,n;
+    cin>>t;
 
-	return 0;
+    for(int i=0;i<t;i++)
+    {
+        string s;
+        Trie* trie = new Trie();
+        cin>>n;
+        vector<string> str(n);
+        for(int j=0;j<n;j++)
+        {
+            cin>>str[j];
+            trie->Insert(str[j], 0);
+        }
+
+        int z=0;
+        for(;z<str.size();z++)
+        {
+            if(trie->Find(str[z],0))
+            {
+                cout<<"NO"<<'\n';
+                break;
+            }
+        }
+        
+        if(z==n)
+            cout<<"YES"<<'\n';
+        delete trie;
+    }
+    
+    return 0;
 }
